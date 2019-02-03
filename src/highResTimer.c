927450c1052cfa7956f9bbe00399884e8296a630
Ömer Eskizara <omereskizara@gmail.com>
2019-02-03 15:45:28 +0300

    highResTimer trials

--- src/highResTimer.c
+++ src/highResTimer.c
@@ -6,18 +6,15 @@
 #include <signal.h>
 #include <time.h>
 
-void print_time (int);
+void print_time (void);
 
 timer_t timer1;
 
 int main (int argc, char **argv)
 {
-    timer_t timer2;
-    struct itimerspec new_value, old_value;
+    struct itimerspec timerSpec, timerSpecOld;
     struct sigaction action;
     struct sigevent sevent;
-    sigset_t set;
-    int signum;
 
     /* SIGALRM for printing time */
     memset (&action, 0, sizeof (struct sigaction));
@@ -34,40 +31,19 @@ int main (int argc, char **argv)
         perror ("timer_create");
 
 
-    new_value.it_interval.tv_sec = 0;
-    new_value.it_interval.tv_nsec = 100000;  /* 100 us */
-    new_value.it_value.tv_sec = 0;
-    new_value.it_value.tv_nsec = 100000;     /* 100 us */
-    if (timer_settime (timer1, 0, &new_value, &old_value) == -1)
+    timerSpec.it_interval.tv_sec = 0;
+    timerSpec.it_interval.tv_nsec = 100000;  /* 100 us */
+    timerSpec.it_value.tv_sec = 0;
+    timerSpec.it_value.tv_nsec = 100000;     /* 100 us */
+    if (timer_settime (timer1, 0, &timerSpec, &timerSpecOld) == -1)
         perror ("timer_settime");
-
-    if (sigemptyset (&set) == -1)
-        perror ("sigemptyset");
-
-    if (sigaddset (&set, SIGRTMIN) == -1)
-        perror ("sigaddset");
-
-    if (sigprocmask (SIG_BLOCK, &set, NULL) == -1)
-        perror ("sigprocmask");
-
-    if (timer_create (CLOCK_MONOTONIC, &sevent, &timer2) == -1)
-        perror ("timer_create");
-
-    new_value.it_interval.tv_sec = 0;
-    new_value.it_interval.tv_nsec = 0;  /* one time timer, no reset */
-    new_value.it_value.tv_sec = 0;
-    new_value.it_value.tv_nsec = 1000000000; /* 1 ms */
-    if (timer_settime (timer2, 0, &new_value, &old_value) == -1)
-        perror ("timer_settime");
-
-    /* wait for completion signal (1 ms)  */
-    if (sigwait (&set, &signum) == -1)
-        perror ("sigwait");
+    
+    while(1);
 
     exit (EXIT_SUCCESS);
 }
 
-void print_time (int signum)
+void print_time (void)
 {   
     struct timespec tp;
     char buffer [80];
